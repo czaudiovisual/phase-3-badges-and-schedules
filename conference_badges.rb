557d61323866341cf35a085a9d2f64ae38d2f859
Christian Bermeo <czaudiovisual@hotmail.com>
2021-08-29 20:54:15 -0400

    commit tree 07c2d3203748caac32dd491d5b2652d2da97f25c

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+badge_maker("Arel")
+
+
+def batch_badge_creator(names)
+    names.map do |name|
+    "Hello, my name is #{name}."
+end
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index(1) do |name, room|
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+end
+end
+
+
+def printer(results)
+    batch_badge_creator(results).each do |badge|
+        puts badge
+    end
+        assign_rooms(results).each do |room|
+        puts room
+        end
+end
